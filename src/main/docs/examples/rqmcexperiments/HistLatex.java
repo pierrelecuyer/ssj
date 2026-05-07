29fb2a04f449c0c21ddb49af51fe9280c0eec8a9
Otmanm <otmans300@gmail.com>
2026-05-07 12:32:15 -0400

    Update ScaledHistogram and histogram LaTeX example

--- src/main/docs/examples/rqmcexperiments/HistLatex.java
+++ src/main/docs/examples/rqmcexperiments/HistLatex.java
@@ -17,14 +17,14 @@ public class HistLatex {
    public static void main(String[] args) throws IOException {
 
       // String directory = "C:/Users/Lecuyer/Dropbox/samo25/paperdat/";  // For testing
-      String directory = "C:/Users/Lecuyer/Dropbox/wsc23/test/";      // For testing
+      String directory = "/home/otman/Documents/GitHub/Data/wsc23/test/";      // For testing
       
       // double xmin = -5.2E-12;
       // double xmax =  5.2E-12;
       double xmin = -1.4E-4;
       double xmax =  1.4E-4;
       int numBins = 40;
-      // int numObs  = 1000000;
+      // int numObs  = 1000000; 
      
       xmax = 1.1E-4;
       xmin = -xmax;
@@ -33,7 +33,10 @@ public class HistLatex {
       hist.fillFromFile(directory + "SmoothPerB4-8-Sob-LMS-RDS-16-1000000.dat");
       // ScaledHistogram histScaled = new ScaledHistogram(hist, 1.0);
       // String histStr = histScaled.toLatex(false, true);
-      System.out.println((new ScaledHistogram(hist, 1.0)).toLatex(true, false));
+      ScaledHistogram Sc_hist = new ScaledHistogram(hist, 1.0);
+      Sc_hist.setAxisOptions("title={SmoothPerB4-8-Sob-LMS-RDS-16}, xlabel={$x$}, ylabel={Density}, width=8cm, height=5cm");
+      Sc_hist.setAddPlotOptions("fill=blue!30, draw=black");
+      System.out.println(Sc_hist.toLatex(true, false));
       
       xmax = 1.25E-6;
       xmin = -xmax;
@@ -41,6 +44,15 @@ public class HistLatex {
       hist.fillFromFile(directory + "MC2-8-Sob-LMS-RDS-16-1000000.dat");
       // histScaled = new ScaledHistogram(hist, 1.0);
       // histStr = histScaled.toLatex(false, true);
-      System.out.println((new ScaledHistogram(hist, 1.0)).toLatex(true, true));
+      Sc_hist = new ScaledHistogram(hist, 1.0);
+
+      Sc_hist.setAxisOptions(
+    		   "title={MC2-8-Sob-LMS-RDS-16}, xlabel={$x$}, ylabel={Density}, " +
+    		   "width=8cm, height=5cm, " +
+    		   "scaled y ticks=false, " +
+    		   "every x tick scale label/.append style={font=\\scriptsize}"
+    		);
+      Sc_hist.setAddPlotOptions("fill=blue!30, draw=black");
+      System.out.println(Sc_hist.toLatex(false, true));
    }
 }
